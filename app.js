9a9dcf875cd30f3755fc813eb9b1e8b64a3104ab
Ronnie Sarma <ronnie.sarma94@gmail.com>
2018-07-21 01:34:07 -0700

    Setup 2

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/api/index');
+var usersRouter = require('./routes/api/users');
 
 var app = express();
 
