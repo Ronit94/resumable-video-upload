a9310062a984e2731cc7a32dc052fe6a3cf81515
Ronnie Sarma <ronnie.sarma94@gmail.com>
2018-07-21 09:20:36 -0700

    Node tus server

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/users', usersRouter);
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type , Access-Token,access-token,regenarate-token,auth_token,Authorization');
   next();
 });
