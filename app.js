1042a15d296c0989a8420903c52a2dccd4105dd2
Ronnie Sarma <ronnie.sarma94@gmail.com>
2018-07-21 01:52:55 -0700

    Add database config fil

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type , Access-Token,access-token,regenarate-token,auth_token,Authorization');
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
