aff55a2a509c393e549370def99bb8e7fb9f6f95
Ronnie Sarma <ronnie.sarma94@gmail.com>
2018-07-21 13:06:00 -0700

    Configuration changes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/api/index');
-var usersRouter = require('./routes/api/users');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var trimRequest=require('trim-request')
-var resumableVideoUploadController=require('./Controllers/resumablevideoUpload')
+var NodeTusServer=require('tus-node-server')
+var fs=require('fs')
+const server = new NodeTusServer.Server();
+
+server.datastore = new NodeTusServer.FileStore({
+  directory: 'files/videos/user-uploads',// this is a temporary folder to save files
+  path: '/videos/tus-upload'// on success of file upload file will place to this directory
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send({"status":1,"message":"Resumable video upload server running"});
 });
-/*
-API end points
-*/
-router.use('/resumable-video-upload',trimRequest.all,resumableVideoUploadController.uploadVideos)
-
 
 module.exports = router;
